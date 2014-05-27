d766ab10feff1555077b88e11f31c678354a2a6b
nikeee13 <nikeee@outlook.com>
2014-05-27 12:48:38 +0200

    Add more Levenshtein, MathEx functions

--- src/NTH/NTH/MathEx.cs
+++ src/NTH/NTH/MathEx.cs
@@ -1,4 +1,6 @@
-﻿namespace NTH
+﻿using System;
+
+namespace NTH
 {
     public static class MathEx
     {
@@ -11,6 +13,29 @@
         {
             var ab = a > b ? a : b;
             return ab > c ? ab : c;
+        }
+
+        public static int Min(params int[] values)
+        {
+            if (values == null || values.Length == 0)
+                throw new ArgumentNullException("values");
+
+            int minVal = values[values.Length - 1];
+            for (int i = values.Length - 2; i != 0; --i)
+                if (values[i] < minVal)
+                    minVal = values[i];
+            return minVal;
+        }
+        public static int Max(params int[] values)
+        {
+            if (values == null || values.Length == 0)
+                throw new ArgumentNullException("values");
+
+            int minVal = values[values.Length - 1];
+            for (int i = values.Length - 2; i != 0; --i)
+                if (values[i] > minVal)
+                    minVal = values[i];
+            return minVal;
         }
     }
 }
--- src/NTH/NTH/Text/LevenshteinCalculator.cs
+++ src/NTH/NTH/Text/LevenshteinCalculator.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace NTH.Text
 {
@@ -81,6 +78,94 @@ namespace NTH.Text
                 }
             }
             return d[currentRow, m];
+        }
+    }
+
+    internal class DamerauLevensteinDistanceMetric
+    {
+        private const int DefaultLength = 255;
+
+        private int[] _currentRow;
+        private int[] _previousRow;
+        private int[] _transpositionRow;
+
+        public DamerauLevensteinDistanceMetric()
+            :this(DefaultLength)
+        { }
+        public DamerauLevensteinDistanceMetric(int maxLength)
+        {
+            _currentRow = new int[maxLength + 1];
+            _previousRow = new int[maxLength + 1];
+            _transpositionRow = new int[maxLength + 1];
+        }
+
+        public int GetDistance(string a, string b, int max)
+        {
+            if (a.IsNullOrEmpty())
+                return b.IsNullOrEmpty() ? 0 : b.Length;
+            else if (b.IsNullOrEmpty())
+                return a.Length;
+
+            if (a.Length > b.Length)
+            {
+                string tmp = a;
+                a = b;
+                b = tmp;
+            }
+
+            int n = a.Length;
+            int m = b.Length;
+
+            if (max < 0)
+                max = m;
+
+            if (m - n > max)
+                return max + 1;
+            if (n > _currentRow.Length)
+            {
+                _currentRow = new int[n + 1];
+                _previousRow = new int[n + 1];
+                _transpositionRow = new int[n + 1];
+            }
+
+            for (int i = 0; i <= n; ++i)
+                _previousRow[i] = i;
+
+            char lastSecondChar = (char)0;
+            for (int i = 1; i <= m; ++i)
+            {
+                char secondChar = b[i - 1];
+                _currentRow[0] = i;
+
+                int from = Math.Max(i - max - 1, 1);
+                int to = Math.Min(i + max + 1, n);
+
+                char lastFirstChar = (char)0;
+                for (int j = from; j <= to; ++j)
+                {
+                    char firstChar = a[j - 1];
+                    int cost = firstChar != secondChar ? 1 : 0;
+                    int value = MathEx.Min(
+                                    _currentRow[j - 1] + 1,
+                                    _previousRow[j] + 1,
+                                    _previousRow[j - 1] + cost
+                                );
+                    if (firstChar == lastSecondChar && secondChar == lastFirstChar)
+                        value = Math.Min(value, _transpositionRow[j - 2] + cost);
+
+                    _currentRow[j] = value;
+                    lastFirstChar = firstChar;
+                }
+
+                lastSecondChar = secondChar;
+
+                int[] tempRow = _transpositionRow;
+                _transpositionRow = _previousRow;
+                _previousRow = _currentRow;
+                _currentRow = tempRow;
+            }
+
+            return _previousRow[n];
         }
     }
 }
