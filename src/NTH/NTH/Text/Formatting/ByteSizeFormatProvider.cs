0e7e5bd9a29a2a06516d9a94f2feea9109dcedcf
nikeee13 <nikeee@outlook.com>
2014-06-07 04:28:02 +0200

    Add formatting provider

---
+++ src/NTH/NTH/Text/Formatting/ByteSizeFormatProvider.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace NTH.Text.Formatting
+{
+    public class ByteSizeFormatProvider : IFormatProvider, ICustomFormatter
+    {
+        public string Format(string format, object arg, IFormatProvider formatProvider)
+        {
+            var argType = arg.GetType();
+            if (argType != typeof(long)
+                && argType != typeof(int)
+                && argType != typeof(short)
+                && argType != typeof(byte))
+            {
+                try
+                {
+                    return HandleOtherFormats(format, arg);
+                }
+                catch (FormatException e)
+                {
+                    throw new FormatException(string.Format("The format of '{0}' is invalid.", format), e);
+                }
+            }
+
+            var byteSize = long.Parse(arg.ToString(), NumberStyles.None, null);
+            return ByteSizeFormatter.FormatBytes(byteSize, false);
+        }
+
+        private string HandleOtherFormats(string format, object arg)
+        {
+            var formattable = arg as IFormattable;
+            if (formattable != null)
+                return formattable.ToString(format, CultureInfo.CurrentCulture);
+            if (arg != null)
+                return arg.ToString();
+            return string.Empty;
+        }
+
+        public object GetFormat(Type formatType)
+        {
+            return formatType == typeof(ICustomFormatter) ? this : null;
+        }
+    }
+}
--- src/NTH/NTH/Text/Formatting/ByteSizeFormatter.cs
+++ src/NTH/NTH/Text/Formatting/ByteSizeFormatter.cs
@@ -6,6 +6,30 @@ namespace NTH.Text.Formatting
     {
         public static string FormatBytes(long byteCount)
         {
+            return FormatBytes(byteCount, false);
+        }
+
+        public static string FormatBytes(long byteCount, bool useBinaryPrefix)
+        {
+            var win = WindowsInternal(byteCount);
+            if (useBinaryPrefix)
+                FixDecimalPrefix(ref win);
+            return win;
+        }
+
+        private static void FixDecimalPrefix(ref string str)
+        {
+            str = str
+                .Replace("KB", "KiB")
+                .Replace("MB", "MiB")
+                .Replace("GB", "GiB")
+                .Replace("TB", "TiB")
+                .Replace("PB", "PiB")
+                .Replace("EB", "EiB");
+        }
+
+        private static string WindowsInternal(long byteCount)
+        {
             var buffer = new StringBuilder(10);
             var res = NativeMethods.StrFormatByteSize(byteCount, buffer, buffer.Capacity);
             return res != null ? buffer.ToString() : string.Empty;
