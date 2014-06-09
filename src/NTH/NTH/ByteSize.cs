e55a358b5dbe14d4d1a5237e330486a43ffcf088
nikeee13 <nikeee@outlook.com>
2014-06-09 02:38:46 +0200

    Add TODO

--- src/NTH/NTH/ByteSize.cs
+++ src/NTH/NTH/ByteSize.cs
@@ -37,6 +37,7 @@ namespace NTH
         }
 
         //TODO: Add Parse/TryParse method
+        //TODO: Add implicit/explicit cast operators
 
         public string ToString(PrefixType prefixType)
         {
