e7a28b34dfcda76b5f46074419b411954059d9bc
nikeee13 <nikeee@outlook.com>
2014-06-10 21:03:15 +0200

    Add CharExtensions

---
+++ src/NTH/NTH/Text/CharExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace NTH.Text
+{
+    public static class CharExtensions
+    {
+        public static bool IsWhiteSpace(this char value)
+        {
+            return char.IsWhiteSpace(value);
+        }
+    }
+}
