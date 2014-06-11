7f55326aaf0b57cb30ba8fc028642becfb7f884e
nikeee <nikeee@outlook.com>
2014-06-11 09:48:06 +0000

    Add StripWhiteSpace

--- src/NTH/NTH/Text/StringExtensions.cs
+++ src/NTH/NTH/Text/StringExtensions.cs
@@ -28,6 +28,19 @@ namespace NTH.Text
 
             return value.GetTypeCode() == TypeCode.DBNull;
         }
+        
+        public static string StripWhiteSpace(this string value)
+        {
+            if(value == null)
+                return null;
+            if(value.Length == 0 || value.Trim().Length == 0)
+                return string.Empty;
+            var sb = new StringBuilder(value.Length);
+            for(int i = 0; i < value.Length; ++i)
+                if(!char.IsWhiteSpace(value[i]))
+                    sb.Append(value[i]);
+            return sb.ToString();
+        }
 
         public static int LevenshteinDistanceTo(this string source, string target)
         {
