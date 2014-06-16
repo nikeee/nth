d066a50977f26f8e0c2a5ba7d0375b12f472120b
nikeee <nikeee@outlook.com>
2014-06-16 09:01:18 +0000

    Add EnumerableExtensions

---
+++ src/NTH/NTH/Collections/Generic/EnumerableExtensions.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NTH.Collections.Generic
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<T> GetPageItems<T>(this IEnumerable<T> items, int page, int itemsPerPage)
+        {
+            if(page < 0)
+                throw new ArgumentException("Argument Page must be greater than or equal to zero.");
+            if(itemsPerPage < 0)
+                throw new ArgumentException("Argument itemsPerPage must be greater than or equal to zero.");
+            return items.Skip(page * itemsPerPage).Take(itemsPerPage);
+        }
+    }
+}
