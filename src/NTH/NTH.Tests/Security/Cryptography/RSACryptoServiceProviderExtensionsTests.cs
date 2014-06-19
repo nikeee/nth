72e86acf7fd629a2a2525321a1f318a8d91bd58d
nikeee13 <nikeee@outlook.com>
2014-06-20 00:40:00 +0200

    Add PEM tests

--- src/NTH/NTH.Tests/Security/Cryptography/RSACryptoServiceProviderExtensionsTests.cs
+++ src/NTH/NTH.Tests/Security/Cryptography/RSACryptoServiceProviderExtensionsTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Security.Cryptography;
 using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -22,7 +23,7 @@ namespace NTH.Tests.Security.Cryptography
             "ut labore et dolore magnam aliquam quaerat voluptatem.";
 
 
-        private static readonly byte[] dataToSign = Encoding.Default.GetBytes(dataToSignString);
+        private static readonly byte[] DataToSign = Encoding.Default.GetBytes(dataToSignString);
 
         [TestMethod]
         public void Der()
@@ -40,7 +41,7 @@ namespace NTH.Tests.Security.Cryptography
                 rsa.PersistKeyInCsp = false;
                 rsa.ImportPrivateKeyDer(privateKeyDER);
                 using (var sha1 = new SHA1CryptoServiceProvider())
-                    signature = rsa.SignData(dataToSign, sha1);
+                    signature = rsa.SignData(DataToSign, sha1);
             }
 
             using (var rsa = new RSACryptoServiceProvider())
@@ -50,14 +51,52 @@ namespace NTH.Tests.Security.Cryptography
 
                 bool isValidSignature;
                 using (var sha1 = new SHA1CryptoServiceProvider())
-                    isValidSignature = rsa.VerifyData(dataToSign, sha1, signature);
+                    isValidSignature = rsa.VerifyData(DataToSign, sha1, signature);
                 Assert.IsTrue(isValidSignature);
 
                 // invalidate signature so the next check must fail
                 signature[signature.Length - 1] ^= 0xFF;
                 using (var sha1 = new SHA1CryptoServiceProvider())
-                    isValidSignature = rsa.VerifyData(dataToSign, sha1, signature);
+                    isValidSignature = rsa.VerifyData(DataToSign, sha1, signature);
                 Assert.IsFalse(isValidSignature);
+            }
+        }
+
+        [TestMethod]
+        public void Pem()
+        {
+            string publicKeyPem = Encoding.ASCII.GetString(Resources.GetResource("RSACryptoServiceProviderExtensionPublicKey.pem"));
+            string privateKeyPem = Encoding.ASCII.GetString(Resources.GetResource("RSACryptoServiceProviderExtensionPrivateKey.pem"));
+
+            Console.WriteLine("Public key:\n{0}", publicKeyPem);
+            Console.WriteLine("Private key:\n{0}", privateKeyPem);
+
+            byte[] signature;
+
+            using (var rsa = new RSACryptoServiceProvider())
+            {
+                rsa.PersistKeyInCsp = false;
+                rsa.ImportPrivateKeyPem(privateKeyPem);
+                using (var sha1 = new SHA1CryptoServiceProvider())
+                    signature = rsa.SignData(DataToSign, sha1);
+            }
+            using (var rsa = new RSACryptoServiceProvider())
+            {
+                rsa.PersistKeyInCsp = false;
+                rsa.ImportPublicKeyPem(publicKeyPem);
+
+                bool isValid;
+
+                using (var sha1 = new SHA1CryptoServiceProvider())
+                    isValid = rsa.VerifyData(DataToSign, sha1, signature);
+
+                Assert.IsTrue(isValid);
+
+                // invalidate signature so the next check must fail
+                signature[signature.Length - 1] ^= 0xFF;
+                using (var sha1 = new SHA1CryptoServiceProvider())
+                    isValid = rsa.VerifyData(DataToSign, sha1, signature);
+                Assert.IsFalse(isValid);
             }
         }
     }
