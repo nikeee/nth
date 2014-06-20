dfe145677d3ed63d801a6daae4bb1fc1bfe54f0c
nikeee13 <nikeee@outlook.com>
2014-06-20 20:32:27 +0200

    Add Extensions

---
+++ src/NTH/NTH/Int32Extensions.cs
@@ -0,0 +1,15 @@
+﻿namespace NTH
+{
+    public static class Int32Extensions
+    {
+        public static string ToHexString(this int i)
+        {
+            return i.ToHexString(true);
+        }
+
+        public static string ToHexString(this int i, bool prefix)
+        {
+            return (prefix ? "0x" : "") + i.ToString("X").PadLeft(8, '0');
+        }
+    }
+}
---
+++ src/NTH/NTH/IntPtrExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NTH
+{
+    public static class IntPtrExtensions
+    {
+        public static string ToHexString(this IntPtr ptr)
+        {
+            return ptr.ToHexString(true);
+        }
+        public static string ToHexString(this IntPtr ptr, bool prefix)
+        {
+            return ptr.ToHexString(prefix, IntPtr.Size);
+        }
+        public static string ToHexString(this IntPtr ptr, bool prefix, int pointerSize)
+        {
+            return (prefix ? "0x" : "") + ptr.ToString("X").PadLeft(2 * pointerSize, '0');
+        }
+    }
+}
