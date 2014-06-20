14a78f0e92d07c500bfb61e2d935e7e24658bf39
nikeee13 <nikeee@outlook.com>
2014-06-21 00:38:19 +0200

    Add proposed Clamp for IComparable<T>

--- src/NTH/NTH/MathEx.cs
+++ src/NTH/NTH/MathEx.cs
@@ -129,6 +129,18 @@ namespace NTH
             return value;
         }
 
+        /*
+        public static T Clamp<T>(T value, T min, T max)
+            where T : IComparable<T>
+        {
+            if (value.CompareTo(min) < 0)
+                return min;
+            if (value.CompareTo(max) > 0)
+                return max;
+            return value;
+        }
+        */
+
         #endregion
 
 
