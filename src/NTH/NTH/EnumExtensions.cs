e88995e6581069fb7e81f9d5be3b1081a72abb61
nikeee13 <nikeee@outlook.com>
2014-06-21 14:57:11 +0200

    Add EnumExtensions

---
+++ src/NTH/NTH/EnumExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NTH
+{
+    public static class EnumExtensions
+    {
+        // TODO: Tests
+        public static T GetAttributeOfType<T>(this Enum value)
+            where T : Attribute
+        {
+            var enumType = value.GetType();
+            var memberInfo = enumType.GetMember(value.ToString());
+            
+            if(memberInfo.Length == 0)
+                throw new ArgumentException("Invalid enum member");
+
+            var attributes = memberInfo[0].GetCustomAttributes(typeof (T), false);
+
+            if (attributes.Length == 0)
+                return null;
+            return attributes[0] as T;
+        }
+    }
+}
