370d4fc0588b21f937720b98f0bb908d124153f1
nikeee13 <nikeee@outlook.com>
2014-06-24 00:45:50 +0200

    Fix indent, Add generic marshalling versions with struct constraint

--- src/NTH/NTH/Runtime/InteropServices/MarshalEx.cs
+++ src/NTH/NTH/Runtime/InteropServices/MarshalEx.cs
@@ -12,7 +12,7 @@ namespace NTH.Runtime.InteropServices
         }
         public static IntPtr AllocStructGlobal<T>(int count)
         {
-            if(count < 1)
+            if (count < 1)
                 throw new ArgumentException("Invalid struct count");
 
             int size = Marshal.SizeOf(typeof(T));
@@ -22,6 +22,18 @@ namespace NTH.Runtime.InteropServices
         public static void FreeStructGlobal<T>(IntPtr hglobal)
         {
             Marshal.FreeHGlobal(hglobal);
+        }
+
+        public static void StructureToPtr<T>(T structure, IntPtr ptr, bool fDeleteOld)
+            where T : struct
+        {
+            Marshal.StructureToPtr(structure, ptr, fDeleteOld);
+        }
+
+        public static void DestroyStructure<T>(IntPtr ptr)
+            where T : struct
+        {
+            Marshal.DestroyStructure(ptr, typeof(T));
         }
     }
 }
