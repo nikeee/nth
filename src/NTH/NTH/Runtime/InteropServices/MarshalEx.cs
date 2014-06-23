e4b85b1f9ada48109fe5d212e6acf2a4a61b7a2c
nikeee13 <nikeee@outlook.com>
2014-06-24 00:02:37 +0200

    Add MarshalEx

---
+++ src/NTH/NTH/Runtime/InteropServices/MarshalEx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace NTH.Runtime.InteropServices
+{
+    public static class MarshalEx
+    {
+        public static IntPtr AllocStructGlobal<T>()
+            where T : struct
+        {
+            return AllocStructGlobal<T>(1);
+        }
+        public static IntPtr AllocStructGlobal<T>(int count)
+        {
+            if(count < 1)
+                throw new ArgumentException("Invalid struct count");
+
+            int size = Marshal.SizeOf(typeof(T));
+            return Marshal.AllocHGlobal(size * count);
+        }
+
+        public static void FreeStructGlobal<T>(IntPtr hglobal)
+        {
+            Marshal.FreeHGlobal(hglobal);
+        }
+    }
+}
