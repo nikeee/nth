742b2f0c91a6e158315e68178e3d148e612d61ea
Niklas Mollenhauer <nikeee@outlook.com>
2014-06-30 00:03:35 +0200

    Add BitUtil class

---
+++ src/NTH/NTH/BitUtil.cs
@@ -0,0 +1,24 @@
+﻿
+namespace NTH
+{
+    public class BitUtil
+    {
+        public static short HiWord(int dword)
+        {
+            return (short)(dword >> 16);
+        }
+        public static short LoWord(int dword)
+        {
+            return (short)(dword & 0x0000FFFF);
+        }
+
+        public static byte HiByte(short word)
+        {
+            return (byte)(word >> 8);
+        }
+        public static byte LoByte(short word)
+        {
+            return (byte)(word & 0x00FF);
+        }
+    }
+}
