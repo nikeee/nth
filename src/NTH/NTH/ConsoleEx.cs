c5e854ec0c66cf20dd51418fd78e882b27857ed2
nikeee13 <nikeee@outlook.com>
2014-06-30 17:40:38 +0200

    Add ConsoleEx

---
+++ src/NTH/NTH/ConsoleEx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Security;
+
+namespace NTH
+{
+    public static class ConsoleEx
+    {
+        private const char DefaultPasswordChar = '*';
+        private const bool DefaultUseMask = false;
+
+        public static SecureString ReadLinePassword()
+        {
+            return ReadLinePassword(DefaultUseMask, DefaultPasswordChar);
+        }
+        public static SecureString ReadLinePassword(bool useMask)
+        {
+            return ReadLinePassword(useMask, DefaultPasswordChar);
+        }
+        public static SecureString ReadLinePassword(bool useMask, char mask)
+        {
+            using (var securePassword = new SecureString())
+            {
+                while (true)
+                {
+                    var k = Console.ReadKey(true);
+
+                    if (k.Key == ConsoleKey.Enter)
+                        break;
+                    if (k.Key == ConsoleKey.Escape)
+                        return null;
+
+                    if (k.Key == ConsoleKey.Backspace)
+                    {
+                        if (securePassword.Length > 0)
+                        {
+                            securePassword.RemoveChar();
+                            Console.Write("\b \b");
+                        }
+                        continue;
+                    }
+
+                    securePassword.AppendChar(k.KeyChar);
+
+                    if (useMask)
+                        Console.Write(mask);
+                }
+
+                if (securePassword.Length > 0)
+                    return securePassword.Copy();
+                return null;
+            }
+        }
+    }
+}
---
+++ src/NTH/NTH/SecureStringExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Security;
+
+namespace NTH
+{
+    internal static class SecureStringExtensions
+    {
+        internal static void RemoveChar(this SecureString value)
+        {
+            if (value == null)
+                throw new NullReferenceException();
+            if (value.Length > 0)
+                value.RemoveAt(value.Length - 1);
+        }
+        internal static string ToUnsecureString(this SecureString value)
+        {
+            if (value == null)
+                throw new NullReferenceException();
+
+            IntPtr bstr = Marshal.SecureStringToBSTR(value);
+
+            try
+            {
+                return Marshal.PtrToStringBSTR(bstr);
+            }
+            finally
+            {
+                Marshal.FreeBSTR(bstr);
+            }
+        }
+    }
+}
