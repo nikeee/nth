514b18d4f430b4ad52652a35b8dc14be0fed9cf5
Niklas Mollenhauer <nikeee@outlook.com>
2014-07-03 00:16:27 +0200

    Add TestHelper

---
+++ src/NTH/NTH.Tests/TestHelper.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace NTH.Tests
+{
+    internal static class TestHelper
+    {
+        internal static bool IsSerializable<T>(T instance)
+        {
+            Assert.IsNotNull(instance);
+
+            using (var ms = new MemoryStream())
+            {
+                var b = new BinaryFormatter();
+                try
+                {
+                    b.Serialize(ms, instance);
+
+                    var instance2 = Deserialize<T>(ms);
+                    Assert.IsNotNull(instance2);
+
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    Assert.Fail(ex.Message);
+                    return false; // Redundant, whatever
+                }
+            }
+        }
+
+        private static T Deserialize<T>(Stream stream)
+        {
+            var bf = new BinaryFormatter();
+            stream.Position = 0;
+            return (T)bf.Deserialize(stream);
+        }
+
+        internal static bool HasAttribute<TType, TAttribute>()
+            where TAttribute : Attribute
+        {
+            var attr = Attribute.GetCustomAttribute(typeof(TType), typeof(TAttribute));
+            Assert.IsNotNull(attr);
+            return attr != null; // Redundant, whatever
+        }
+    }
+}
