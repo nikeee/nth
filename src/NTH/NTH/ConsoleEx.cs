9ebf0d4abc04405e258105b33bc39d96e652b24b
Niklas Mollenhauer <nikeee@outlook.com>
2014-07-03 01:02:26 +0200

    ReadLinePassword -> ReadLineMasked

--- src/NTH/NTH/ConsoleEx.cs
+++ src/NTH/NTH/ConsoleEx.cs
@@ -8,15 +8,15 @@ namespace NTH
         private const char DefaultPasswordChar = '*';
         private const bool DefaultUseMask = false;
 
-        public static SecureString ReadLinePassword()
+        public static SecureString ReadLineMasked()
         {
-            return ReadLinePassword(DefaultUseMask, DefaultPasswordChar);
+            return ReadLineMasked(DefaultUseMask, DefaultPasswordChar);
         }
-        public static SecureString ReadLinePassword(bool useMask)
+        public static SecureString ReadLineMasked(bool useMask)
         {
-            return ReadLinePassword(useMask, DefaultPasswordChar);
+            return ReadLineMasked(useMask, DefaultPasswordChar);
         }
-        public static SecureString ReadLinePassword(bool useMask, char mask)
+        public static SecureString ReadLineMasked(bool useMask, char mask)
         {
             using (var securePassword = new SecureString())
             {
