059a61be0066b9ea98a5e957434b9e657dd4c76e
Niklas Mollenhauer <nikeee@outlook.com>
2015-04-03 23:58:18 +0200

    Perf optimization

--- src/NTH/NTH/BitConverterEx.cs
+++ src/NTH/NTH/BitConverterEx.cs
@@ -9,7 +9,15 @@ namespace NTH
         {
             if (value == null)
                 return null;
-            return BitConverter.ToString(value).Replace("-", "");
+            var len = value.Length * 2;
+            var chars = new char[len];
+            for (int i = 0, index = 0; i < len; i += 2)
+            {
+                var currentByte = value[index++];
+                chars[i] = GetHexCharForInt(currentByte / 0x10);
+                chars[i + 1] = GetHexCharForInt(currentByte % 0x10);
+            }
+            return new string(chars, 0, len);
         }
 
         public static byte[] FromHexString(string value)
@@ -33,16 +41,24 @@ namespace NTH
 
             for (int i = 0; i < value.Length >> 1; ++i)
             {
-                res[i] = (byte)((GetHexValue(value[i << 1]) << 4) + (GetHexValue(value[(i << 1) + 1])));
+                res[i] = (byte)((GetIntForHexChar(value[i << 1]) << 4) + (GetIntForHexChar(value[(i << 1) + 1])));
             }
 
             return res;
         }
 
-        private static int GetHexValue(char hex)
+        private static int GetIntForHexChar(char hex)
         {
             // See: http://stackoverflow.com/a/9995303/785210
             return hex - (hex < 58 ? 48 : 55);
+        }
+
+        private static char GetHexCharForInt(int i)
+        {
+            Debug.Assert(i >= 0 && i < 16, "i is out of range.");
+            if (i < 10)
+                return (char)(i + '0');
+            return (char)(i - 10 + 'A');
         }
     }
 }
