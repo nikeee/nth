50b3cae0abea0ab6bb7a40af144f122156bf641f
Niklas Mollenhauer <nikeee@users.noreply.github.com>
2015-08-29 04:10:43 +0200

    Fix C&P typo

--- src/NTH/NTH/PrefixType.cs
+++ src/NTH/NTH/PrefixType.cs
@@ -6,7 +6,7 @@
         Unknown = 0x0,
         /// <summary>Decimal prefix. See <see cref="https://en.wikipedia.org/wiki/Binary_prefix">Wikipedia</see>.</summary>
         Decimal = 0x1,
-        /// <summary>Decimal prefix. See <see cref="https://en.wikipedia.org/wiki/Binary_prefix">Wikipedia</see>.</summary>
+        /// <summary>Binary prefix. See <see cref="https://en.wikipedia.org/wiki/Binary_prefix">Wikipedia</see>.</summary>
         Binary = 0x2
     }
 }
